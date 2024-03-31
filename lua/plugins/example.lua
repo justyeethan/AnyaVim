a9bb70c5b20088decd93eb2bc49a7e4e8eedeb51
Ethan Y. <theethan5753@gmail.com>
2024-03-31 10:05:33 -0700

    nvim config update 03/31/2024

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -129,6 +129,18 @@ if true then
     -- Configure LazyVim to load gruvbox
     { "ayu-theme/ayu-vim" },
     {
+      "projekt0n/github-nvim-theme",
+      lazy = false, -- make sure we load this during startup if it is your main colorscheme
+      priority = 1000, -- make sure to load this before all the other start plugins
+      config = function()
+        require("github-theme").setup({
+          -- ...
+        })
+
+        vim.cmd("colorscheme github_dark_default")
+      end,
+    },
+    {
       "nvimdev/dashboard-nvim",
       event = "VimEnter",
       opts = function()
@@ -219,9 +231,9 @@ return {
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
+    --     opts = {
+    --       colorscheme = "ayu",
+    --     },
   },
 
   -- change trouble config
