6d056fef479ef4618de49755b8f921c6f9d946f4
Ethan <diamondspectreme@gmail.com>
2024-05-05 09:08:57 -0700

    Remove annoying update pop-up

--- lua/plugins/plugin.lua
+++ lua/plugins/plugin.lua
@@ -141,6 +141,20 @@ if true then
       end,
     },
     {
+      "ray-x/go.nvim",
+      dependencies = { -- optional packages
+        "ray-x/guihua.lua",
+        "neovim/nvim-lspconfig",
+        "nvim-treesitter/nvim-treesitter",
+      },
+      config = function()
+        require("go").setup()
+      end,
+      event = { "CmdlineEnter" },
+      ft = { "go", "gomod" },
+      build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    },
+    {
       "nvimdev/dashboard-nvim",
       event = "VimEnter",
       opts = function()
