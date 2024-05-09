5c7e5e80734f6d9903b1e5b9cd18db29d1bd3af6
Ethan Y. <theethan5753@gmail.com>
2024-05-08 23:58:31 -0700

    Add buffer switching

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,8 @@ local map = LazyVim.safe_keymap_set
 
 -- use jk to exit insert mode
 map("i", "jk", "<ESC>", { desc = "Exit insert mode" })
+
+-- Buffer move
+map("", "<leader>n", ":bnext<cr>", { desc = "Buffer next" })
+map("", "<leader>p", ":bprevious<cr>", { desc = "Buffer previous" })
+map("", "<leader>d", ":bdelete<cr>", { desc = "Buffer delete" })
