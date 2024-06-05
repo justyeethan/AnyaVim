5b65705a8add65a42c8e301862731599a4333a0f
Ethan <diamondspectreme@gmail.com>
2024-06-04 20:48:07 -0700

    move neotree to the right

--- lua/plugins/plugin.lua
+++ lua/plugins/plugin.lua
@@ -130,6 +130,14 @@ if true then
     -- Configure LazyVim to load gruvbox
     { "ayu-theme/ayu-vim" },
     {
+      "nvim-neo-tree/neo-tree.nvim",
+      opts = {
+        window = {
+          position = "right",
+        },
+      },
+    },
+    {
       "projekt0n/github-nvim-theme",
       lazy = false, -- make sure we load this during startup if it is your main colorscheme
       priority = 1000, -- make sure to load this before all the other start plugins
